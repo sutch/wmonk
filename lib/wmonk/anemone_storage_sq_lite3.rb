d362a43851bf0802a9c284bb25963604195b6171
Dennis Sutch <dennis@sutch.com>
2013-07-31 22:12:08 -0400

    Initial release

---
+++ lib/wmonk/anemone_storage_sq_lite3.rb
@@ -0,0 +1,3 @@
+class AnemoneStorageSQLite3
+  # To change this template use File | Settings | File Templates.
+end
---
+++ lib/wmonk/server.rb
@@ -0,0 +1,3 @@
+class Server
+  # To change this template use File | Settings | File Templates.
+end
